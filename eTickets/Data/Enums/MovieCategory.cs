c6026ad527d80ce9422ba2cb48c11e241fbe6bcf
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-07-22 15:45:24 +0430

    Models was added

---
+++ eTickets/Data/Enums/MovieCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace eTickets.Data.Enums;
+
+public enum MovieCategory
+{
+    Action = 1,
+    Comedy,
+    Drama,
+    Documentary
+}
--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,6 +1,10 @@
+using eTickets.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
